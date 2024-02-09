4db3bf71fd047458a23a9ab9af460a56ea4aeab1
IbaiBuR <ibaibur@chessnibble.com>
2024-02-09 18:35:31 +0100

    Add functions to push pawns

--- src/movegen.c
+++ src/movegen.c
@@ -1,1 +1,13 @@
 #include "movegen.h"
+
+#include "bitboard.h"
+
+inline Bitboard singlePawnPush(Bitboard pawns, Bitboard empty, Color c) {
+    return (c == WHITE) ? shiftN(pawns) & empty : shiftS(pawns) & empty;
+}
+
+inline Bitboard doublePawnPush(Bitboard pawns, Bitboard emtpy, Color c) {
+    return (c == WHITE)
+             ? shiftN(singlePawnPush(pawns, emtpy, WHITE)) & emtpy & rank4
+             : shiftS(singlePawnPush(pawns, emtpy, BLACK)) & emtpy & rank5;
+}
--- src/movegen.h
+++ src/movegen.h
@@ -1,1 +1,9 @@
 #pragma once
+
+#include "types.h"
+
+Bitboard singlePawnPush(Bitboard bitboard, Bitboard empty, Color color);
+Bitboard doublePawnPush(Bitboard bitboard, Bitboard empty, Color color);
+
+void generateMoves(MoveList *moveList, Board *board);
+
