035c3d710603618e6215451b4d5073a8373f82eb
IbaiBuR <ibaibur@chessnibble.com>
2024-02-15 00:52:28 +0100

    Appy clang-format

--- src/movegen.h
+++ src/movegen.h
@@ -6,4 +6,3 @@ Bitboard singlePawnPush(Bitboard bitboard, Bitboard empty, Color color);
 Bitboard doublePawnPush(Bitboard bitboard, Bitboard empty, Color color);
 
 void generateMoves(MoveList *moveList, Board *board);
-
