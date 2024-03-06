aaf7554d091df60063d0b29a297dbd341e81db64
IbaiBuR <ibaibur@chessnibble.com>
2024-03-06 12:13:52 +0100

    Basics for move encoding

---
+++ src/move.c
@@ -0,0 +1,13 @@
+#include "move.h"
+
+#include <stdio.h>
+
+#include "board.h"
+
+char * moveToString(const Move move) {
+    static char buffer[6];
+
+    sprintf(buffer, "%s%s", squareToCoordinates[from(move)], squareToCoordinates[to(move)]);
+
+    return buffer;
+}
---
+++ src/move.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "types.h"
+
+#define makeMove(from,to,flag) ((from) | ((to) << 6) | (flag))
+#define from(move) ((move & 0x3F))
+#define to(move) (((move >> 6) & 0x3F))
+
+char * moveToString(Move move);
