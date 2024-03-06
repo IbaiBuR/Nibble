4d415d6bf021d420d8a57348eb17f59ce255eca9
IbaiBuR <ibaibur@chessnibble.com>
2024-03-06 01:53:37 +0100

    Format code with clang-format

--- src/movegen.c
+++ src/movegen.c
@@ -7,11 +7,13 @@ INLINE void addMove(const Move move, MoveList *moveList) {
     moveList->count++;
 }
 
-static inline Bitboard singlePawnPush(const Bitboard pawns, const Bitboard empty, const Color c) {
+static inline Bitboard
+singlePawnPush(const Bitboard pawns, const Bitboard empty, const Color c) {
     return (c == WHITE) ? shiftN(pawns) & empty : shiftS(pawns) & empty;
 }
 
-static inline Bitboard doublePawnPush(const Bitboard pawns, const Bitboard empty, const Color c) {
+static inline Bitboard
+doublePawnPush(const Bitboard pawns, const Bitboard empty, const Color c) {
     return (c == WHITE)
              ? shiftN(singlePawnPush(pawns, empty, WHITE)) & empty & rank4
              : shiftS(singlePawnPush(pawns, empty, BLACK)) & empty & rank5;
