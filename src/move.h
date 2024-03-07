3c7c074e8103ae0723613fc88c34d47c85bcea04
IbaiBuR <ibaibur@chessnibble.com>
2024-03-07 02:27:43 +0100

    Add MoveFlags to prepare for movegen

--- src/move.h
+++ src/move.h
@@ -2,8 +2,22 @@
 
 #include "types.h"
 
-#define makeMove(from, to, flag) ((from) | ((to) << 6) | (flag))
+#define makeMove(from, to, flag) ((from) | (to << 6) | (flag << 12))
 #define from(move)               ((move & 0x3F))
 #define to(move)                 (((move >> 6) & 0x3F))
+#define flag(move)               (((move >> 12) & 0x0F))
+
+enum MoveFlags {
+    NOMOVE,
+    QUIET,
+    CAPTURE,
+    ENPASSANT,
+    CASTLE,
+    DOUBLEPUSH,
+    KNIGHT_PROMO,
+    BISHOP_PROMO,
+    ROOK_PROMO,
+    QUEEN_PROMO
+};
 
 char *moveToString(Move move);
