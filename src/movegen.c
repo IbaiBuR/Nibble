c6b302a442bcc0cbb9386b10c22744e220037a0a
IbaiBuR <ibaibur@chessnibble.com>
2024-03-22 20:43:22 +0100

    Quiet moves: Done!

--- src/movegen.c
+++ src/movegen.c
@@ -16,8 +16,8 @@ void initializeMoveList(MoveList *moveList) {
     moveList->moves->score = 0;
 }
 
-static inline void quietPawnMoves(const Board *board, MoveList *moveList, const Color c) {
-    const Bitboard  empty     = ~board->occupancies[COLOR_NB];
+static inline void
+quietPawnMoves(const Board *board, MoveList *moveList, const Color c, const Bitboard empty) {
     const Bitboard  pawns     = pieceBB(board, PAWN, c);
     const Direction movingDir = c == WHITE ? SOUTH : NORTH;
 
@@ -34,5 +34,36 @@ static inline void quietPawnMoves(const Board *board, MoveList *moveList, const 
     {
         const Square to = popLsb(&doublePush);
         addMove(makeMove(to - movingDir * 2, to, DOUBLEPUSH), moveList);
+    }
+}
+
+static inline void quietPieceMoves(const Board    *board,
+                                   MoveList       *moveList,
+                                   const Color     c,
+                                   const PieceType pt,
+                                   const Bitboard  emtpy) {
+    Bitboard pieces = pieceBB(board, pt, c);
+
+    while (pieces)
+    {
+        const Square from = popLsb(&pieces);
+        Bitboard     possiblePieceMoves =
+            getAttacksByPieceType(pt, from, board->occupancies[COLOR_NB]) & emtpy;
+
+        while (possiblePieceMoves)
+        {
+            addMove(makeMove(from, popLsb(&possiblePieceMoves), QUIET), moveList);
+        }
+    }
+}
+
+inline void generateAllQuiets(const Board *board, MoveList *moveList, const Color c) {
+    const Bitboard empty = ~board->occupancies[COLOR_NB];
+
+    quietPawnMoves(board, moveList, c, empty);
+
+    for (PieceType pt = KNIGHT; pt <= KING; pt++)
+    {
+        quietPieceMoves(board, moveList, c, pt, empty);
     }
 }
--- src/movegen.h
+++ src/movegen.h
@@ -3,3 +3,4 @@
 #include "types.h"
 
 void initializeMoveList(MoveList *moveList);
+void generateAllQuiets(const Board *board, MoveList *moveList, Color color);
