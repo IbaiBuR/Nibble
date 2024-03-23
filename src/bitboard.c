b956c430015c95cc8981d2a819cf49262bd3a8c3
IbaiBuR <ibaibur@chessnibble.com>
2024-03-23 22:37:28 +0100

    Fix getAttacksByPieceType function

--- src/bitboard.c
+++ src/bitboard.c
@@ -220,28 +220,19 @@ Bitboard setOccupancies(const int index, const int nBits, Bitboard attacks) {
 Bitboard getAttacksByPieceType(const PieceType pt, const Square sq, const Bitboard blockers) {
     switch (pt)
     {
-    case PAWN:
-        return pawnAttacks[sq];
-        break;
     case KNIGHT:
         return knightAttacks[sq];
-        break;
     case KING:
         return kingAttacks[sq];
-        break;
     case BISHOP:
         return getBishopAttacks(sq, blockers);
-        break;
     case ROOK:
         return getRookAttacks(sq, blockers);
-        break;
     case QUEEN:
         return getBishopAttacks(sq, blockers) | getRookAttacks(sq, blockers);
-        break;
     default:
         printf("Invalid piece type.\n");
         return 0ULL;
-        break;
     }
 }
 
