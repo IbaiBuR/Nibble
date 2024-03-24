929d100af43c6b4ca663708ca9629448de6c801f
IbaiBuR <ibaibur@chessnibble.com>
2024-03-24 02:42:45 +0100

    Parse promotions accordingly in moveToString function

--- src/move.c
+++ src/move.c
@@ -4,10 +4,25 @@
 
 #include "board.h"
 
+bool isPromotion(const Move move) {
+    const int moveFlag = flag(move);
+
+    return moveFlag == KNIGHT_PROMO || moveFlag == BISHOP_PROMO || moveFlag == ROOK_PROMO
+        || moveFlag == QUEEN_PROMO;
+}
+
 char *moveToString(const Move move) {
     static char buffer[6];
 
-    sprintf(buffer, "%s%s", squareToCoordinates[from(move)], squareToCoordinates[to(move)]);
+    if (isPromotion(move))
+    {
+        sprintf(buffer, "%s%s%c", squareToCoordinates[from(move)], squareToCoordinates[to(move)],
+                promoFlagToChar[flag(move)]);
+    }
+    else
+    {
+        sprintf(buffer, "%s%s", squareToCoordinates[from(move)], squareToCoordinates[to(move)]);
+    }
 
     return buffer;
 }
--- src/move.h
+++ src/move.h
@@ -3,9 +3,9 @@
 #include "types.h"
 
 #define buildMove(from, to, flag) ((from) | (to << 6) | (flag << 12))
-#define from(move)               ((move & 0x3F))
-#define to(move)                 (((move >> 6) & 0x3F))
-#define flag(move)               (((move >> 12) & 0x0F))
+#define from(move)                ((move & 0x3F))
+#define to(move)                  (((move >> 6) & 0x3F))
+#define flag(move)                (((move >> 12) & 0x0F))
 
 enum MoveFlags {
     NOMOVE,
@@ -19,5 +19,10 @@ enum MoveFlags {
     ROOK_PROMO,
     QUEEN_PROMO
 };
+
+constexpr char promoFlagToChar[] = {
+    [KNIGHT_PROMO] = 'n', [BISHOP_PROMO] = 'b', [ROOK_PROMO] = 'r', [QUEEN_PROMO] = 'q'};
+
+bool isPromotion(Move move);
 
 char *moveToString(Move move);
