30022d405daf33378175b3672606249825c823c4
IbaiBuR <ibaibur@chessnibble.com>
2024-03-30 15:26:36 +0100

    Add perfTest function

--- src/perft.c
+++ src/perft.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 #include "bitboard.h"
-#include "board.h"
 #include "move.h"
 #include "movegen.h"
+#include "util.h"
 
 uint64_t nodes;
 
@@ -30,6 +30,38 @@ void perftDriver(const int depth, Board *board) {
 
         undoMove(board);
     }
+}
 
-    printf("nodes: %ld\n", nodes);
+uint64_t perftTest(const int depth, Board *board) {
+    nodes = 0ULL;
+    MoveList moveList;
+
+    generateAllMoves(board, &moveList, board->stm);
+
+    const uint64_t start = getTimeMS();
+
+    for (uint32_t i = 0; i < moveList.count; i++)
+    {
+        const Move currentMove = moveList.moves[i].move;
+
+        if (!makeMove(currentMove, board))
+            continue;
+
+        const uint64_t cumulatedNodes = nodes;
+
+        perftDriver(depth - 1, board);
+
+        undoMove(board);
+
+        const uint64_t old = nodes - cumulatedNodes;
+
+        printf(" %s: %ld\n", moveToString(currentMove), old);
+    }
+
+    printf("\n");
+    printf(" Depth: %d\n", depth);
+    printf(" Nodes: %ld\n", nodes);
+    printf(" Time : %ld\n\n", getTimeMS() - start);
+
+    return nodes;
 }
--- src/perft.h
+++ src/perft.h
@@ -2,4 +2,5 @@
 
 #include "types.h"
 
-void perftDriver(int depth, Board *board);
+void     perftDriver(int depth, Board *board);
+uint64_t perftTest(int depth, Board *board);
