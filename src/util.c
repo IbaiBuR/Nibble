58b382706ae4a7c158db0c13548a78da4cdb48a5
IbaiBuR <ibaibur@chessnibble.com>
2024-04-08 02:20:02 +0200

    Remove unused functions

--- src/util.c
+++ src/util.c
@@ -6,31 +6,6 @@
     #include <sys/time.h>
 #endif
 
-static uint32_t seed = 1804289383;
-
-// XORShift algorithm
-uint32_t randU32() {
-    uint32_t num = seed;
-
-    num ^= num << 13;
-    num ^= num >> 17;
-    num ^= num << 5;
-
-    seed = num;
-
-    return num;
-}
-
-uint64_t randU64() {
-
-    const uint64_t n1 = (uint64_t)(randU32()) & 0xFFFF;
-    const uint64_t n2 = (uint64_t)(randU32()) & 0xFFFF;
-    const uint64_t n3 = (uint64_t)(randU32()) & 0xFFFF;
-    const uint64_t n4 = (uint64_t)(randU32()) & 0xFFFF;
-
-    return n1 | (n2 << 16) | (n3 << 32) | (n4 << 48);
-}
-
 long getTimeMS() {
 #ifdef WIN64
     return GetTickCount();
--- src/util.h
+++ src/util.h
@@ -1,11 +1,6 @@
 #pragma once
 
-#include <inttypes.h>
-
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
-
-uint32_t randU32();
-uint64_t randU64();
 
 long getTimeMS();
